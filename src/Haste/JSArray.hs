061a7aba94265c6d723e799bf76d3542dcdd9d73
Kirill Kouzoubov <Kirill888@gmail.com>
2015-04-19 15:49:45 +1000

    Making things compile with haste-0.5
    New version of haste renamed some types and functions
    Pack   ---> FromAny
    UnPack ---> ToAny

--- src/Haste/JSArray.hs
+++ src/Haste/JSArray.hs
@@ -13,7 +13,7 @@ import Prelude hiding ((!), (++), concat, concatMap, drop, fmap, length)
 import qualified Prelude as P (fmap, return)
 import System.IO.Unsafe (unsafePerformIO)
 
-newtype JSArray a = JSArray JSAny deriving (Pack, Unpack)
+newtype JSArray a = JSArray JSAny deriving (FromAny, ToAny)
 
 newJSArray::IO (JSArray a)
 newJSArray = ffi "(function() {return [];})"
@@ -24,14 +24,14 @@ newJSArray = ffi "(function() {return [];})"
     plus :: JSArray a->JSArray a->IO (JSArray a)
     plus = ffi "(function(a, b) {return a.concat(b);})"
 
-head::Pack a=>JSArray a->IO (Maybe a)
+head::FromAny a=>JSArray a->IO (Maybe a)
 head arr = do
   len <- length arr
   case len of
     0 -> return Nothing
     _ -> Just <$> arr ! 0
 
-last::Pack a=>JSArray a->IO (Maybe a)
+last::FromAny a=>JSArray a->IO (Maybe a)
 last arr = do
   len <- length arr
   case len of
@@ -50,16 +50,16 @@ null = liftM (0==) . length
 length::JSArray a->IO Int
 length = ffi "(function(arr) {return arr.length;})"
 
-fmap::(Pack a, Unpack b, NFData a)=>(a->b)->JSArray a->JSArray b
+fmap::(FromAny a, ToAny b, NFData a)=>(a->b)->JSArray a->JSArray b
 fmap f xs = fromList . P.fmap f . toList $ xs
 
 reverse::JSArray a->JSArray a
 reverse = unsafePerformIO . ffi "(function(arr) {var result = arr.slice();result.reverse();return result;})"
 
-foldl::(Pack a, Pack b, Unpack b)=>(b->a->b)->b->JSArray a->b
+foldl::(FromAny a, FromAny b, ToAny b)=>(b->a->b)->b->JSArray a->b
 foldl f x0 arr = unsafePerformIO $ foldl_ (\x y -> return $ f x y) x0 arr
   where
-    foldl_::(Pack a, Pack b, Unpack b)=>(b->a->IO b)->b->JSArray a->IO b
+    foldl_::(FromAny a, FromAny b, ToAny b)=>(b->a->IO b)->b->JSArray a->IO b
     foldl_ = ffi "(function(f, x0, arr) {return arr.reduce(function(cumulant, cur) {return f(cumulant, cur);});})"
 
 concat::JSArray (JSArray a)->JSArray a
@@ -68,16 +68,16 @@ concat = unsafePerformIO . ffi "(function(arrs) {return Array.prototype.concat.a
 concat'::[JSArray a]->JSArray a
 concat' = foldl' (++) (unsafePerformIO newJSArray)
 
-concatMap::(Pack a, NFData a)=>(a->JSArray b)->JSArray a->JSArray b
+concatMap::(FromAny a, NFData a)=>(a->JSArray b)->JSArray a->JSArray b
 concatMap f = concat . fmap f
 
-concatMap'::(Pack a, Unpack b, NFData a)=>(a->[b])->JSArray a->JSArray b
+concatMap'::(FromAny a, ToAny b, NFData a)=>(a->[b])->JSArray a->JSArray b
 concatMap' f = concatMap (fromList . f)
 
-replicate::Unpack a=>Int->a->JSArray a
+replicate::ToAny a=>Int->a->JSArray a
 replicate n x = unsafePerformIO $ replicate' n x
   where
-    replicate'::Unpack a=>Int->a->IO (JSArray a)
+    replicate'::ToAny a=>Int->a->IO (JSArray a)
     replicate' = ffi "(function(n, x) {var result = [];for(var i = 0;i < n; ++i) {result.push(x)};return result;})"
 
 slice::Int->Int->JSArray a->JSArray a
@@ -92,27 +92,27 @@ take = slice 0
 drop::Int->JSArray a->JSArray a
 drop n arr = slice n (unsafePerformIO $ length arr) arr
 
-push::Unpack a=>JSArray a->a->IO ()
+push::ToAny a=>JSArray a->a->IO ()
 push = ffi "(function(arr, val) {arr.push(val);})"
 
-pop::Pack a=>JSArray a->IO a
+pop::FromAny a=>JSArray a->IO a
 pop = ffi "(function(arr) {return arr.pop();})"
 
-(!)::Pack a=>JSArray a->Int->IO a
+(!)::FromAny a=>JSArray a->Int->IO a
 (!) = ffi "(function(arr, idx) {return arr[idx];})"
 
-zipWith::(Pack a, Pack b, Unpack c)=>(a->b->c)->JSArray a->JSArray b->JSArray c
+zipWith::(FromAny a, FromAny b, ToAny c)=>(a->b->c)->JSArray a->JSArray b->JSArray c
 zipWith f xs ys = unsafePerformIO $ zipWith' (\x y -> return $ f x y) xs ys
   where
-    zipWith'::(Pack a, Pack b, Unpack c)=>(a->b->IO c)->JSArray a->JSArray b->IO (JSArray c)
+    zipWith'::(FromAny a, FromAny b, ToAny c)=>(a->b->IO c)->JSArray a->JSArray b->IO (JSArray c)
     zipWith' = ffi "(function(f, xs, ys) {var result = [];for (var i = 0;i < Math.min(xs.length, ys.length); ++i) {result.push(f(xs[i], ys[i]));};return result})"
 
-fromList::Unpack a=>[a]->JSArray a
+fromList::ToAny a=>[a]->JSArray a
 fromList vals = unsafePerformIO $ do
   arr <- newJSArray
   mapM_ (push arr) vals
   return arr
 
-toList::(Pack a, NFData a)=>JSArray a->[a]
+toList::(FromAny a, NFData a)=>JSArray a->[a]
 toList arr = force $ P.fmap (unsafePerformIO . (arr !))
              [0..(unsafePerformIO . length $ arr) - 1]
--- src/Haste/JSArray/Typed.hs
+++ src/Haste/JSArray/Typed.hs
@@ -9,7 +9,7 @@ import qualified Haste.JSArray as JA
 import Haste.Prim
 import System.IO.Unsafe (unsafePerformIO)
 
-newtype ArrayBuffer = ArrayBuffer JSAny deriving (Pack, Unpack)
+newtype ArrayBuffer = ArrayBuffer JSAny deriving (FromAny, ToAny)
 
 newArrayBuffer::Int->IO ArrayBuffer
 newArrayBuffer = ffi "(function(byteLength) {return ArrayBuffer(byteLength);})"
@@ -17,11 +17,11 @@ newArrayBuffer = ffi "(function(byteLength) {return ArrayBuffer(byteLength);})"
 arrayBufferLength::ArrayBuffer->Int
 arrayBufferLength = unsafePerformIO . ffi "(function(buf) {return buf.byteLength;})"
 
-class (Pack a, Unpack a)=>TypedArray a where
+class (FromAny a, ToAny a)=>TypedArray a where
   type EltType a
   newSizedArray::Int->IO a
   copyTypedArray::TypedArray b=>b->a
-  fromList::Unpack (EltType a)=>[EltType a]->a
+  fromList::ToAny (EltType a)=>[EltType a]->a
   viewArrayBufferSized::Int->Int->ArrayBuffer->a
   viewArrayBuffer::ArrayBuffer->a
   fromJSArray::JA.JSArray (EltType a)->a
@@ -33,17 +33,17 @@ class (Pack a, Unpack a)=>TypedArray a where
   arrayLength::a->Int
   arrayLength arr = quot (byteLength arr) (bytesPerElement arr)
 
-  getIndex::Pack (EltType a)=>a->Int->IO (EltType a)
+  getIndex::FromAny (EltType a)=>a->Int->IO (EltType a)
   getIndex = ffi "(function(arr, idx) {return arr[idx];})"
-  setIndex::Unpack (EltType a)=>a->Int->EltType a->IO ()
+  setIndex::ToAny (EltType a)=>a->Int->EltType a->IO ()
   setIndex = ffi "(function(arr, idx, val) {arr[idx] = val;})"
-  toList::(Pack (EltType a), NFData (EltType a))=>a->[EltType a]
+  toList::(FromAny (EltType a), NFData (EltType a))=>a->[EltType a]
   toList arr = force $ fmap (unsafePerformIO . getIndex arr)
                [0..arrayLength arr - 1]
   toJSArray::a->JA.JSArray (EltType a)
   toJSArray = unsafePerformIO . ffi "(function(arr) {return Array.prototype.slice.call(arr);})"
 
-newtype Int8Array = Int8Array JSAny deriving (Pack, Unpack)
+newtype Int8Array = Int8Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Int8Array where
   type (EltType Int8Array) = Int
@@ -57,7 +57,7 @@ instance TypedArray Int8Array where
   viewArrayBuffer = unsafePerformIO . ffi "(function(buffer) {return new Int8Array(buffer);})"
   fromJSArray = unsafePerformIO . ffi "(function(buffer) {return new Int8Array(buffer);})"
 
-newtype Int16Array = Int16Array JSAny deriving (Pack, Unpack)
+newtype Int16Array = Int16Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Int16Array where
   type (EltType Int16Array) = Int
@@ -71,7 +71,7 @@ instance TypedArray Int16Array where
   viewArrayBuffer = unsafePerformIO . ffi "(function(buffer) {return new Int16Array(buffer);})"
   fromJSArray = unsafePerformIO . ffi "(function(buffer) {return new Int16Array(buffer);})"
 
-newtype Int32Array = Int32Array JSAny deriving (Pack, Unpack)
+newtype Int32Array = Int32Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Int32Array where
   type (EltType Int32Array) = Int
@@ -85,7 +85,7 @@ instance TypedArray Int32Array where
   viewArrayBuffer = unsafePerformIO . ffi "(function(buffer) {return new Int32Array(buffer);})"
   fromJSArray = unsafePerformIO . ffi "(function(buffer) {return new Int32Array(buffer);})"
 
-newtype Int64Array = Int64Array JSAny deriving (Pack, Unpack)
+newtype Int64Array = Int64Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Int64Array where
   type (EltType Int64Array) = Int
@@ -99,7 +99,7 @@ instance TypedArray Int64Array where
   viewArrayBuffer = unsafePerformIO . ffi "(function(buffer) {return new Int64Array(buffer);})"
   fromJSArray = unsafePerformIO . ffi "(function(buffer) {return new Int64Array(buffer);})"
 
-newtype Uint8Array = Uint8Array JSAny deriving (Pack, Unpack)
+newtype Uint8Array = Uint8Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Uint8Array where
   type (EltType Uint8Array) = Int
@@ -113,7 +113,7 @@ instance TypedArray Uint8Array where
   viewArrayBuffer = unsafePerformIO . ffi "(function(buffer) {return new Uint8Array(buffer);})"
   fromJSArray = unsafePerformIO . ffi "(function(buffer) {return new Uint8Array(buffer);})"
 
-newtype Uint16Array = Uint16Array JSAny deriving (Pack, Unpack)
+newtype Uint16Array = Uint16Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Uint16Array where
   type (EltType Uint16Array) = Int
@@ -127,7 +127,7 @@ instance TypedArray Uint16Array where
   viewArrayBuffer = unsafePerformIO . ffi "(function(buffer) {return new Uint16Array(buffer);})"
   fromJSArray = unsafePerformIO . ffi "(function(buffer) {return new Uint16Array(buffer);})"
 
-newtype Uint32Array = Uint32Array JSAny deriving (Pack, Unpack)
+newtype Uint32Array = Uint32Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Uint32Array where
   type (EltType Uint32Array) = Int
@@ -141,7 +141,7 @@ instance TypedArray Uint32Array where
   viewArrayBuffer = unsafePerformIO . ffi "(function(buffer) {return new Uint32Array(buffer);})"
   fromJSArray = unsafePerformIO . ffi "(function(buffer) {return new Uint32Array(buffer);})"
 
-newtype Float32Array = Float32Array JSAny deriving (Pack, Unpack)
+newtype Float32Array = Float32Array JSAny deriving (FromAny, ToAny)
 
 instance TypedArray Float32Array where
   type (EltType Float32Array) = Double
